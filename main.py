8784f76ff3ddea14fd1c0fbcbe78a658cfcfb090
t73434008 <t73434008@gmail.com>
2025-07-14 00:42:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -92,32 +92,7 @@ class MainScraper:
                 drive_saver.upload_file(file, folder_id)
                 print(f"Uploaded {file} to Google Drive in folder ID: {folder_id}.")
 
-    # def upload_to_drive(self):
-    #     """
-    #     Upload saved Excel files to Google Drive in a folder named with yesterday's date.
-    #     """
-    #     if not self.excel_files:
-    #         print("No Excel files to upload.")
-    #         return
-
-    #     # Initialize SavingOnDrive
-    #     if 'BOSHAMLAN_GCLOUD_KEY_JSON' not in os.environ:
-    #         raise EnvironmentError("BOSHAMLAN_GCLOUD_KEY_JSON not found.")
-
-    #     credentials_json = os.environ['BOSHAMLAN_GCLOUD_KEY_JSON']
-    #     credentials_dict = json.loads(credentials_json)
-
-    #     drive_saver = SavingOnDrive(credentials_dict)
-    #     drive_saver.authenticate()
-
-    #     parent_folder_id = "11sji-ooCJdIx3t10rtr6mB0O9BntAJoF"
-    #     folder_id = drive_saver.create_folder(self.yesterday, parent_folder_id)
-    #     print(f"Created folder '{self.yesterday}' with ID: {folder_id}")
-
-    #     for file in self.excel_files:
-    #         drive_saver.upload_file(file, folder_id)
-    #         print(f"Uploaded {file} to Google Drive.")
-
+  
 if __name__ == "__main__":
     main_scraper = MainScraper()
     asyncio.run(main_scraper.run())
